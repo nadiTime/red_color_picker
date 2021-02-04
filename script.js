1989a4b67d2e73942f91ee5eec75527aca72feff
nadiTime <nadavcn@gmail.com>
2021-02-04 16:37:20 +0200

    Move to class

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Instanciate class with  picker size in pixels
 const redColorPicker = new RedColorPicker(101, 101);
 const container = document.querySelector('#color-container');
+
+// Add picker to page
 redColorPicker.addPicker(container);
